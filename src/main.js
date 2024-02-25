3be020b7bc66b2f4a7577cb68e2380f7a561a17b
Ebube Rebecca <ebubeokeke5@outlook.com>
2024-02-25 14:15:21 +0100

    register page update

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import "ant-design-vue/dist/reset.css";
 import App from './App.vue'
 import { DatePicker } from 'ant-design-vue';
-import { Select, ConfigProvider, Button, Tag, Menu, Dropdown, Avatar, Spin, Result, Space, Form, Tabs, Input, Checkbox, Row, Col, Modal, Alert, Divider, Upload, PageHeader, Progress } from 'ant-design-vue';
+import { Select, ConfigProvider, Button, Tag, Menu, Dropdown, Avatar, Spin, Result, Space, Form, Tabs, Input, Checkbox, Row, Col, Modal, Alert, Divider, Upload, PageHeader, Progress, Radio, RadioGroup } from 'ant-design-vue';
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import axios from 'axios'
@@ -45,6 +45,8 @@ app.use(Select)
 app.use(Upload)
 app.use(PageHeader)
 app.use(Progress)
+app.use(Radio)
+app.use(RadioGroup)
 
 
 
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,89 +6,65 @@
             <a-col :span="8"></a-col>
             <a-col :span="8">
                 <a-space direction="vertical" style="width: 100%">
+
                     <h3>Registration Form</h3>
                     <a-form layout="vertical">
 
+
+                        <a-form-item ref="name" label="Full name" name="name">
+                            <a-input placeholder="Full Name" size="large" v-model:value="name" />
+                        </a-form-item>
+
                         <a-form-item ref="email" label="Email" name="email">
-                            <a-input v-model:value="value" placeholder="Email" size="large" />
+                            <a-input placeholder="Email" size="large" v-model:value="email" />
                         </a-form-item>
 
                         <a-form-item ref="password" label="Password" name="password">
-                            <a-input placeholder="Password" size="large" />
+                            <a-input placeholder="Password" size="large" type="password" v-model:value="password" />
                         </a-form-item>
-                        <a-form-item ref="user_role" label="User Role" name="user_role">
 
-                            <a-select ref="select" v-model:value="value1" style="width: 200px" size="large">
-                                <a-select-option value="chef">Chef</a-select-option>
-                                <a-select-option value="foodie">Foodie</a-select-option>
-                            </a-select>
+
+
+                        <a-form-item label="User role" name="user_role">
+                            <a-radio-group name="radioGroup" v-model:value="user_role">
+                                <a-radio value="chef">Chef</a-radio>
+                                <a-radio value="recipe_seeker">Recipe Seeker</a-radio>
+
+                            </a-radio-group>
                         </a-form-item>
+
                         <a-form-item>
                             <a-button type="primary" html-type="submit" value="large" size="large"
                                 class="blue-register-button">Register</a-button>
                         </a-form-item>
+
                     </a-form>
+
+
                 </a-space>
             </a-col>
             <a-col :span="8"></a-col>
         </a-row>
+        <a-row></a-row>
 
 
     </div>
 </template>
 <script>
-
-
-import { defineComponent, watch, ref } from 'vue';
-
+import { defineComponent, ref } from 'vue';
 export default defineComponent({
-    setup() {
-        const options1 = ref([
-            {
-                value: 'Chef',
-                label: 'Chef',
-            },
-            {
-                value: 'password',
-                label: 'password',
-            },
-            {
-                value: 'disabled',
-                label: 'Disabled',
-                disabled: true,
-            },
-            {
-                value: 'yiminghe',
-                label: 'Yiminghe',
-            },
-        ]);
-        const options2 = ref([
-            {
-                value: 'lucy',
-                label: 'Lucy',
-            },
-        ]);
-        const options3 = ref([
-            {
-                value: 'lucy',
-                label: 'Lucy',
-            },
-        ]);
-        const focus = () => {
-            console.log('focus');
-        };
-        const handleChange = value => {
-            console.log(`selected ${value}`);
-        };
+    data() {
         return {
-            focus,
-            handleChange,
-            value1: ref('lucy'),
-            value2: ref('lucy'),
-            value3: ref('lucy'),
-            options1,
-            options2,
-            options3,
+            user_role: '',
+            name: '',
+            email: '',
+            password: ''
+        }
+    },
+    setup() {
+
+        return {
+
         };
     },
 });
